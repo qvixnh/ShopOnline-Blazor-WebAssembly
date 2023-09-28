a3299255c9588497d706f6ca63afef145d8756ff
qvixnh <nguyenvinh22012002@gmail.com>
2023-09-28 15:05:08 +0700

    Create ProductRepository Class - repository design pattern

--- ShopOnline.Models/Dtos/CartItemDto.cs
+++ ShopOnline.Models/Dtos/CartItemDto.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ShopOnline.Models.Dtos
 {
-    internal class CartItemDto
+    public  class CartItemDto
     {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImageURL { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public int Qty { get; set; }
+
     }
 }
