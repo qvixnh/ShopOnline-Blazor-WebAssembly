3c59a7e3b0e4acee628e5f6b842c09fd02331c0b
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-04 15:30:48 +0700

    Intergration of Paypal Payment Gateway into Blazor Component

--- ShopOnline.Web/HardCoded.cs
+++ ShopOnline.Web/HardCoded.cs
@@ -7,3 +7,81 @@
 
     }
 }
+//Card Type : Visa
+//Card Number: 41373351171713899
+//Expiration Date: 07 / 2027
+//CVV: 620
+
+//"id": "6US67652AS7210221",
+//  "intent": "CAPTURE",
+//  "status": "COMPLETED",
+//  "purchase_units": [
+//    {
+//      "reference_id": "default",
+//      "amount": {
+//        "currency_code": "USD",
+//        "value": "350.00"
+//      },
+//      "payee": {
+//    "email_address": "sb-ro8w027141510@business.example.com",
+//        "merchant_id": "V75PJ6EZ7QCNW"
+//      },
+//      "description": "O_1_e85dd3d2-2721-419f-98ab-0b6441e08896",
+//      "soft_descriptor": "PAYPAL *TEST STORE",
+//      "shipping": {
+//    "name": {
+//        "full_name": "Bob Jones"
+//        },
+//        "address": {
+//        "address_line_1": "Adress1",
+//          "address_line_2": "Address2",
+//          "admin_area_2": "Wimbledon",
+//          "admin_area_1": "LONDON",
+//          "postal_code": "SW196AY",
+//          "country_code": "GB"
+//        }
+//},
+//      "payments": {
+//    "captures": [
+//          {
+//        "id": "3UJ32809XU717792S",
+//            "status": "COMPLETED",
+//            "amount": {
+//            "currency_code": "USD",
+//              "value": "350.00"
+//            },
+//            "final_capture": true,
+//            "seller_protection": {
+//            "status": "ELIGIBLE",
+//              "dispute_categories": [
+//                "ITEM_NOT_RECEIVED",
+//                "UNAUTHORIZED_TRANSACTION"
+//              ]
+//            },
+//            "create_time": "2023-10-04T08:11:06Z",
+//            "update_time": "2023-10-04T08:11:06Z"
+//          }
+//        ]
+//      }
+//    }
+//  ],
+//  "payer": {
+//    "name": {
+//        "given_name": "Bob",
+//      "surname": "Jones"
+//    },
+//    "email_address": "bobjones@hotmail.co.za",
+//    "payer_id": "2G5MQZVYP539C",
+//    "address": {
+//        "country_code": "GB"
+//    }
+//},
+//  "create_time": "2023-10-04T08:08:10Z",
+//  "update_time": "2023-10-04T08:11:06Z",
+//  "links": [
+//    {
+//      "href": "https://api.sandbox.paypal.com/v2/checkout/orders/6US67652AS7210221",
+//      "rel": "self",
+//      "method": "GET"
+//    }
+//  ]
