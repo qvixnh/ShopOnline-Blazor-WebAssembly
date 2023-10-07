7e064b82c96bf2c3ecb686e4576103a2e1789e28
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-07 10:58:35 +0700

    UserLocalStorageFunctionality - Add Service

--- ShopOnline.Web/Program.cs
+++ ShopOnline.Web/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShopOnline.Web;
@@ -12,5 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7189/") });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
-
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<IManageCartItemsLocalStorageService, ManageCartItemsLocalStorageService>();
+builder.Services.AddScoped<IManageProductsLocalStorageService, ManageProductsLocalStorageService>();
 await builder.Build().RunAsync();
---
+++ ShopOnline.Web/Services/ManageCartItemsLocalStorageService.cs
@@ -0,0 +1,40 @@
+﻿using Blazored.LocalStorage;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Services
+{
+    public class ManageCartItemsLocalStorageService : IManageCartItemsLocalStorageService
+    {
+        private readonly ILocalStorageService localStorageService;
+        private readonly IShoppingCartService shoppingCartService;
+        private const string key = "ManageCartItemsCollection";
+
+
+        public ManageCartItemsLocalStorageService(ILocalStorageService localStorageService, IShoppingCartService shoppingCartService)
+        {
+            this.localStorageService = localStorageService;
+            this.shoppingCartService = shoppingCartService;
+        }
+        public Task<List<CartItemDto>> GetCollection()
+        {
+            return await this.localStorageService.GetItemAsync<IEnumerable<ProductDto>>(key)
+                ?? await AddCollection();
+        }
+
+        public Task RemoveCollection()
+        {
+            throw new NotImplementedException();
+        }
+        public Task SaveCollection(List<CartItemDto> cartItemDtos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<List<CartItemDto>> AddCollection()
+        {
+            var shoppingCartCollection = await shoppingCartService.GetItems(HardCoded.UserId);
+
+        }
+    }
+}
